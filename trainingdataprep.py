2a4d916c7474ce2a8abe12d86c598b7e3e722971
Cathy Deng <cathyd125@gmail.com>
2014-07-18 09:43:10 -0500

    prep test data

--- trainingdataprep.py
+++ trainingdataprep.py
@@ -1,31 +1,24 @@
 import pycrfsuite
 
+def parseLines(lines):
+	parsed = [[]]
+	addr_index = 0
+	token_index = 0
+	tag_list = ['', 'street number', '2', 'street', '4', 'city', 'state', 'zip']
 
-file = open('./training_data/us50.train.tagged', 'r')
-lines = file.readlines()
-
-addr_training = [[]]
-
-addr_index = 0
-token_index = 0
-
-tag_list = ['', 'street number', '2', 'street', '4', 'city', 'state', 'zip']
-
-for line in lines:
-	if line == '\n':
-		addr_index += 1
-		token_index = 0
-		addr_training.append([])
-	else:
-		split = line.split(' |')
-		token_string = split[0]
-		token_num = split[1].rstrip()
-		token_num = int(token_num)
-		token_tag = tag_list[token_num]
-		addr_training[addr_index].append((token_string, token_tag))
-
-print addr_training #test
-
+	for line in lines:
+		if line == '\n':
+			addr_index += 1
+			token_index = 0
+			parsed.append([])
+		else:
+			split = line.split(' |')
+			token_string = split[0]
+			token_num = split[1].rstrip()
+			token_num = int(token_num)
+			token_tag = tag_list[token_num]
+			parsed[addr_index].append((token_string, token_tag))
+	return parsed
 
 def word2features(address, i):
 	word = address[i][0]
@@ -39,15 +32,23 @@ def addr2labels(address):
 	return [address[i][1] for i in range(len(address))]
 
 
-#print addr2features(addr_training[0]) #test
-#print addr2labels(addr_training[0]) #test
+# prep the training & test data
+file = open('./training_data/us50.train.tagged', 'r')
+train_lines = file.readlines()
+train_data = parseLines(train_lines)
+
+x_train = [addr2features(addr) for addr in train_data]
+y_train = [addr2labels(addr) for addr in train_data]
+
+file = open('./training_data/us50.test.tagged', 'r')
+test_lines = file.readlines()
+test_data = parseLines(test_lines)
+
+x_test = [addr2features(addr) for addr in test_data]
+y_test = [addr2labels(addr) for addr in test_data]
 
 
-x_train = [addr2features(addr) for addr in addr_training]
-y_train = [addr2labels(addr) for addr in addr_training]
-
-#print zip(x_train, y_train) #test
-
+#train model
 trainer = pycrfsuite.Trainer(verbose=False)
 
 for xseq, yseq in zip(x_train, y_train):
