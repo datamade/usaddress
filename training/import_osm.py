6d51b0aa6e18d0cd99485861d60a7f20f40cb97a
Cathy Deng <cathyd125@gmail.com>
2014-08-13 10:18:15 -0500

    tweak osm import query

--- training/import_osm.py
+++ training/import_osm.py
@@ -17,10 +17,10 @@ query1 = """<union>
 </union>
 
 <print/>""" % ((-86.805193, 47.080799, 42.491920, -92.889259) * 2)
-r1 = requests.post('http://overpass-api.de/api/interpreter/', data=query1)
+#r1 = requests.post('http://overpass-api.de/api/interpreter/', data=query1)
 
-f = open("data/osm_data.xml", "w")
-f.write(r1.text)
+#f = open("data/osm_data.xml", "w")
+#f.write(r1.text)
 
 
 query2 = """<union>
@@ -43,18 +43,18 @@ query2 = """<union>
 <print/>""" % ((-87.61309146881104, 41.890042371392965, 41.87234107841773, -87.64235973358154) * 2)
 r2 = requests.post('http://overpass-api.de/api/interpreter/', data=query2)
 
-f = codecs.open("data/osm_data_street.xml", "wb", "utf-8")
-r2.encoding = 'utf-8'
-f.write(r2.text)
+#f = codecs.open("data/osm_data_street.xml", "wb", "utf-8")
+#r2.encoding = 'utf-8'
+#f.write(r2.text)
 
 
 query3 = """<union>
 <query type="way">
-  <has-kv k="addr:full" regv="^[0-9]+.*[a-z]+.*[0-9]+.*"/>
+  <has-kv k="addr:full" regv="^[0-9]+.*[a-z]+.*[0-9]{5}.*"/>
   <bbox-query e="%s" n="%s" s="%s" w="%s"/>
   </query>
 <query type="node">
-  <has-kv k="addr:full" regv="^[0-9]+.*[a-z]+.*[0-9]+.*"/>
+  <has-kv k="addr:full" regv="^[0-9]+.*[a-z]+.*[0-9]{5}.*"/>
   <bbox-query e="%s" n="%s" s="%s" w="%s"/>
 </query>
 </union>
