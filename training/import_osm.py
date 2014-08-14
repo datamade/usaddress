c27b30c766aa59a58a906f0afd723638df26fb28
Cathy Deng <cathyd125@gmail.com>
2014-08-14 14:11:22 -0500

    change query for synthetic osm addresses, add data

--- training/import_osm.py
+++ training/import_osm.py
@@ -3,24 +3,55 @@ import codecs
 
 query1 = """<union>
 <query type="way">
-  <has-kv k="addr:full"/> 
-  <has-kv k="addr:city"/>
-  <has-kv k="addr:street"/>
+  <has-kv k="addr:housenumber"/>
+  <has-kv k="addr:street:name"/>
+  <has-kv k="addr:street:type"/>
+  <has-kv k="addr:state"/>
   <bbox-query e="%s" n="%s" s="%s" w="%s"/>
-  </query>
-<query type="node">
-  <has-kv k="addr:full"/> 
+</query>
+<query type="way">
+  <has-kv k="addr:housenumber"/>
+  <has-kv k="addr:street:name"/>
+  <has-kv k="addr:street:type"/>
   <has-kv k="addr:city"/>
-  <has-kv k="addr:street"/>
+  <bbox-query e="%s" n="%s" s="%s" w="%s"/>
+</query>
+<query type="way">
+  <has-kv k="addr:housenumber"/>
+  <has-kv k="addr:street:name"/>
+  <has-kv k="addr:street:type"/>
+  <has-kv k="addr:postcode"/>
+  <bbox-query e="%s" n="%s" s="%s" w="%s"/>
+</query>
+<query type="node">
+  <has-kv k="addr:housenumber"/>
+  <has-kv k="addr:street:name"/>
+  <has-kv k="addr:street:type"/>
+  <has-kv k="addr:state"/>
+  <bbox-query e="%s" n="%s" s="%s" w="%s"/>
+</query>
+<query type="node">
+  <has-kv k="addr:housenumber"/>
+  <has-kv k="addr:street:name"/>
+  <has-kv k="addr:street:type"/>
+  <has-kv k="addr:city"/>
+  <bbox-query e="%s" n="%s" s="%s" w="%s"/>
+</query>
+<query type="node">
+  <has-kv k="addr:housenumber"/>
+  <has-kv k="addr:street:name"/>
+  <has-kv k="addr:street:type"/>
+  <has-kv k="addr:postcode"/>
   <bbox-query e="%s" n="%s" s="%s" w="%s"/>
 </query>
 </union>
 
-<print/>""" % ((-86.805193, 47.080799, 42.491920, -92.889259) * 2)
-#r1 = requests.post('http://overpass-api.de/api/interpreter/', data=query1)
+<print/>""" % ((-70.000000, 50.000000, 25.000000, -125.000000) * 6)
+r1 = requests.post('http://overpass-api.de/api/interpreter/', data=query1)
+r1.encoding = 'utf-8'
 
-#f = open("data/osm_data.xml", "w")
-#f.write(r1.text)
+f = codecs.open('data/osm_data.xml', encoding='utf-8' , mode='w+')
+f.write(r1.text)
 
 
 query2 = """<union>
@@ -41,7 +72,7 @@ query2 = """<union>
 </union>
 
 <print/>""" % ((-87.61309146881104, 41.890042371392965, 41.87234107841773, -87.64235973358154) * 2)
-r2 = requests.post('http://overpass-api.de/api/interpreter/', data=query2)
+#r2 = requests.post('http://overpass-api.de/api/interpreter/', data=query2)
 
 #f = codecs.open("data/osm_data_street.xml", "wb", "utf-8")
 #r2.encoding = 'utf-8'
@@ -61,8 +92,8 @@ query3 = """<union>
 
 <print/>
 """ % ((-70.000000, 50.000000, 25.000000, -125.000000) * 2)
-r3 = requests.post('http://overpass-api.de/api/interpreter/', data=query3)
+#r3 = requests.post('http://overpass-api.de/api/interpreter/', data=query3)
 
-f = codecs.open("data/osm_data_full_addr.xml", "wb", "utf-8")
-r3.encoding = 'utf-8'
-f.write(r3.text)
+#f = codecs.open("data/osm_data_full_addr.xml", "wb", "utf-8")
+#r3.encoding = 'utf-8'
+#f.write(r3.text)
