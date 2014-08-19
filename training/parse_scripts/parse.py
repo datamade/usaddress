bfde74da15bf98c27e9aae96069fa0c4c2378eea
Cathy Deng <cathyd125@gmail.com>
2014-08-19 16:39:08 -0500

    convert tabs to spaces

--- training/parse_scripts/parse.py
+++ training/parse_scripts/parse.py
@@ -5,58 +5,58 @@ import random
 
 # osm xml data -> list of dicts representing osm addresses
 def xmlToAddrList(xml_file):
-	tree = etree.parse(xml_file)
-	root = tree.getroot()
-	addr_list=[]
-	for element in root:
-		if element.tag == 'node' or element.tag =='way':
-			address={}
-			for x in element.iter('tag'):
-				addr = ast.literal_eval(str(x.attrib))
-				address[addr['k']]=addr['v']
-			addr_list.append(address)
-	return addr_list
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+    addr_list=[]
+    for element in root:
+        if element.tag == 'node' or element.tag =='way':
+            address={}
+            for x in element.iter('tag'):
+                addr = ast.literal_eval(str(x.attrib))
+                address[addr['k']]=addr['v']
+            addr_list.append(address)
+    return addr_list
 
 
 # natural addresses (in addr:full from osm xml data) -> training file (xml)
 def osmNaturalToTraining(xml_file):
-	address_list = xmlToAddrList(xml_file)
-	train_addr_list = etree.Element('AddressCollection')
-	trainFileName = '../training_data/'+re.sub(r'\W+', '_', xml_file)+'.xml'
-	punc_list = ',.'
-	# only the osm tags below will end up in training data; others will be ignored
-	osm_tags_to_addr_tags = {
-		"addr:housenumber":"AddressNumber",
-		"addr:street:prefix":"StreetNamePreDirectional",
-		"addr:street:name":"StreetName",
-		"addr:street:type":"StreetNamePostType",
-		"addr:city":"PlaceName",
-		"addr:state":"StateName",
-		"addr:postcode":"ZipCode"}
-	for address in address_list:
-		addr_tokens = address['addr:full'].split()
-		train_addr = etree.Element('AddressString')
-		is_addr_taggable = True
-		#loop through tokens & find tags for each
-		for token in addr_tokens:
-			is_token_taggable = False
-			for key, value in address.items():
-				if key in osm_tags_to_addr_tags.keys() and key != 'addr:full' and token in value.split():
-					is_taggable = True
-					token_xml = etree.Element(osm_tags_to_addr_tags[key])
-					#check for punctuation
-					token_xml.text = token
-					if token[-1] in punc_list:
-						token_xml.text = token[0:-1]
-						token_xml.tail = token[-1]
-					train_addr.append(token_xml)
-			if is_token_taggable ==False:
-				is_addr_taggable = False
-		if is_addr_taggable == True:
-			train_addr_list.append(train_addr)
-	output = etree.tostring(train_addr_list, pretty_print=True)
-	with open(trainFileName, 'w') as f:
-		f.write(output)
+    address_list = xmlToAddrList(xml_file)
+    train_addr_list = etree.Element('AddressCollection')
+    trainFileName = '../training_data/'+re.sub(r'\W+', '_', xml_file)+'.xml'
+    punc_list = ',.'
+    # only the osm tags below will end up in training data; others will be ignored
+    osm_tags_to_addr_tags = {
+        "addr:housenumber":"AddressNumber",
+        "addr:street:prefix":"StreetNamePreDirectional",
+        "addr:street:name":"StreetName",
+        "addr:street:type":"StreetNamePostType",
+        "addr:city":"PlaceName",
+        "addr:state":"StateName",
+        "addr:postcode":"ZipCode"}
+    for address in address_list:
+        addr_tokens = address['addr:full'].split()
+        train_addr = etree.Element('AddressString')
+        is_addr_taggable = True
+        #loop through tokens & find tags for each
+        for token in addr_tokens:
+            is_token_taggable = False
+            for key, value in address.items():
+                if key in osm_tags_to_addr_tags.keys() and key != 'addr:full' and token in value.split():
+                    is_taggable = True
+                    token_xml = etree.Element(osm_tags_to_addr_tags[key])
+                    #check for punctuation
+                    token_xml.text = token
+                    if token[-1] in punc_list:
+                        token_xml.text = token[0:-1]
+                        token_xml.tail = token[-1]
+                    train_addr.append(token_xml)
+            if is_token_taggable ==False:
+                is_addr_taggable = False
+        if is_addr_taggable == True:
+            train_addr_list.append(train_addr)
+    output = etree.tostring(train_addr_list, pretty_print=True)
+    with open(trainFileName, 'w') as f:
+        f.write(output)
 
 # osm xml data -> synthetic addresses -> training file (xml)
 def osmSyntheticToTraining(xml_file):
@@ -86,7 +86,7 @@ def osmSyntheticToTraining(xml_file):
                     token_xml.text = word
                     token_xml.tail = ' '
                     components[tag_type].append(token_xml)
-		
+        
         for tag_type in ('Street','City', 'Area') :
             l = components[tag_type]
             if l :
@@ -95,7 +95,7 @@ def osmSyntheticToTraining(xml_file):
         address_xml = (components['Street'] 
                        + components['City'] 
                        + components['Area'])
-		
+        
         address_xml[-1].tail = None
 
         for xml_element in address_xml:
@@ -123,36 +123,36 @@ osmSyntheticToTraining('../data/osm_data.xml')
 
 # us50 data -> training file (xml)
 def trainFileFromLines(addr_file):
-	lines = open(addr_file, 'r')
-	addr_index = 0
-	token_index = 0
-	trainFileName = '../training_data/'+re.sub(r'\W+', '_', addr_file)+'.xml'
+    lines = open(addr_file, 'r')
+    addr_index = 0
+    token_index = 0
+    trainFileName = '../training_data/'+re.sub(r'\W+', '_', addr_file)+'.xml'
 
 
-	tag_list = [None, 'AddressNumber', 'USPSBox', 'StreetName', 'StreetNamePostType',
+    tag_list = [None, 'AddressNumber', 'USPSBox', 'StreetName', 'StreetNamePostType',
                 'PlaceName', 'StateName', 'ZipCode', 'suffix']
-	addr_list = etree.Element('AddressCollection')
-	addr = etree.Element('AddressString')
-	for line in lines:
-		if line =='\n':
-			addr_index += 1
-			token_index = 0
-			addr_list.append(addr)
-			addr = etree.Element('AddressString')
-		else:
-			split = line.split(' |')
-			token_string = split[0]
-			token_num = int(split[1].rstrip())
-			token_tag = tag_list[token_num]
-			token_xml = etree.Element(token_tag)
-			token_xml.text = token_string
-		addr.append(token_xml)
+    addr_list = etree.Element('AddressCollection')
+    addr = etree.Element('AddressString')
+    for line in lines:
+        if line =='\n':
+            addr_index += 1
+            token_index = 0
+            addr_list.append(addr)
+            addr = etree.Element('AddressString')
+        else:
+            split = line.split(' |')
+            token_string = split[0]
+            token_num = int(split[1].rstrip())
+            token_tag = tag_list[token_num]
+            token_xml = etree.Element(token_tag)
+            token_xml.text = token_string
+        addr.append(token_xml)
 
 
 
-	output = etree.tostring(addr_list, pretty_print=True)
-	with open(trainFileName, 'w') as f:
-		f.write(output)
+    output = etree.tostring(addr_list, pretty_print=True)
+    with open(trainFileName, 'w') as f:
+        f.write(output)
 
 
 
