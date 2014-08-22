3ddea04084997d64a09e46c4a325c48ad55090cb
Cathy Deng <cathyd125@gmail.com>
2014-08-22 17:16:53 -0500

    cli for tagging addresses: pre-tag strings w/ usaddress, fix user tagging, improve display of tokens/tags, move xml handling

--- training/manual_labeling.py
+++ training/manual_labeling.py
@@ -1,4 +1,4 @@
-import pycrfsuite
+import usaddress
 from lxml import etree
 import sys
 
@@ -8,11 +8,20 @@ def consoleLabel(raw_addr_list, label_options): #should this also take the model
     tagged_addr_list = []
     punc_list = ['.', ',']
     valid_input_tags = []
+    label_dict = {}
+
     for i in range(len(label_options)):
         valid_input_tags.append(str(i))
 
+    for label in label_options:
+        label_dict[label[0]] = label[1]
+        label_dict[label[1]] = label[0]
+
+    addr_index = 0
+    total_addrs = len(raw_addr_list)
     while not finished:
         for addr_string in raw_addr_list:
+            addr_index += 1
 
             addr_tokens = addr_string.split()
 
@@ -21,45 +30,64 @@ def consoleLabel(raw_addr_list, label_options): #should this also take the model
             user_input_yn = ''
             user_input_tag = ''
 
-            #tag addr tokens using model here
+            print "("+str(addr_index)+" of "+str(total_addrs)+") ", "-"*50
+            print "ADDRESS STRING: ", addr_string
+            #tag addr tokens using model
+            preds = usaddress.parse(addr_string)
+            # ** check that the predicted labels match up with those in label_options?
 
             while not valid_yn:
 
-                print addr_string
+                # pretty print for address tokens & tags
+                for pred in preds:
+                    n = 12-len(pred[0])
+                    print pred[0], ' '*n, '|',
+                print "\n",
+                for pred in preds:
+                    if pred[1] == 'Null':
+                        print_tag = 'punc' # ** this could be smarter
+                    else:
+                        print_tag = label_dict[pred[1]]
+                    n = 12-len(print_tag)
+                    print print_tag, ' '*n, '|',
                 #print predicted tags here
+                print "\n\n",
 
                 sys.stderr.write('Is this correct? (y)es / (n)o / (s)kip\n')
                 user_input_yn = sys.stdin.readline().strip()
-                if user_input_yn[0] in ['y', 'n', 's']:
+                if user_input_yn in ['y', 'n', 's', '']:
                     valid_yn = True
 
-            if user_input_yn[0] =='y':
-                print "OK"
-                # generate addr_xml based on predicted tags & append to tagged_address_list
-                # would we only want to add addresses that were predicted incorrectly?
 
-            if user_input_yn[0] =='n':
+            if user_input_yn =='y':
+                tagged_addr=[]
+                for token in preds:
+                    tagged_addr.append((token[0], token[1]))
+                tagged_addr_list.append(tagged_addr)
+
+            elif user_input_yn =='n':
                 while not valid_tag:
-                    addr_xml = etree.Element('AddressString')
-                    for token in addr_tokens:
+                    tagged_addr = []
+                    for token in preds:
                         valid_tag = False
                         while not valid_tag:
-                            print 'What is \''+token+'\' ?' #where should the tag list be printed?
+                            print 'What is \''+token[0]+'\' ? If '+token[1]+' hit return' #where should the tag list be printed?
                             user_input_tag = sys.stdin.readline().strip()
-                            if user_input_tag in valid_input_tags:
+                            if user_input_tag in valid_input_tags or user_input_tag == '':
                                 valid_tag = True
 
-                        token_xml = etree.Element(label_options[int(user_input_tag)][1])
-                        punc = ''
-                        if token[-1] in punc_list:
-                            punc = token[-1]
-                            token = token[:-1]
-                        token_xml.text = token
-                        token_xml.tail = punc + ' '
-                        addr_xml.append(token_xml)
+                        xml_tag = ''
+                        if user_input_tag == '':
+                            xml_tag = token[1]
+                        else:
+                            xml_tag = label_options[int(user_input_tag)][1]
 
-                addr_xml[-1].tail = None
-                tagged_addr_list.append(addr_xml)
+                        tagged_addr.append((token[0], xml_tag))
+
+                tagged_addr_list.append(tagged_addr)
+
+            else:
+                print "Skipped\n"
 
         print "Done! Yay!"
         finished = True
@@ -67,14 +95,27 @@ def consoleLabel(raw_addr_list, label_options): #should this also take the model
     return tagged_addr_list
 
 
-def XMLtoFile (xml_list, filepath):
+def list2XMLfile(addr_list, filepath):
     address_list_xml = etree.Element('AddressCollection')
-    address_list_xml.extend(xml_list)
+
+    for addr in addr_list:
+        addr_xml = etree.Element('AddressString')
+        xml_token_list = []
+        for token in addr:
+            if token[1] == None or token[1] == 'Null': #make this smarter
+                if len(xml_token_list) > 0:
+                    xml_token_list[-1].tail = token[0] + ' '
+            else:
+                token_xml = etree.Element(token[1])
+                token_xml.text = token[0]
+                token_xml.tail = ' '
+                xml_token_list.append(token_xml)
+        xml_token_list[-1].tail = ''
+        addr_xml.extend(xml_token_list)
+        address_list_xml.append(addr_xml)
+
     with open( filepath, 'w' ) as f:
         f.write(etree.tostring(address_list_xml, pretty_print = True))
-
-
-
 
 
 
@@ -87,7 +128,7 @@ sample_addr_list = [
 
 # a list of labels, w/ each label represented as [label display name, label xml tag]
 labels = [
-    ['punctuation', None],
+    ['punc', None],
     ['addr no', 'AddressNumber'],
     ['street dir', 'StreetNamePreDirectional'],
     ['street name', 'StreetName'],
@@ -102,5 +143,5 @@ labels = [
     ]
 
 tagged_addr_list = consoleLabel( sample_addr_list, labels )
-XMLtoFile( tagged_addr_list, 'training_data/TESTINGcommandline.xml' )
+list2XMLfile( tagged_addr_list, 'training_data/TESTINGcommandline.xml' )
 
