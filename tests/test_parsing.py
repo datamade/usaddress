46fd463795ff4261a0899a5d8a272e9fdf2b72e8
Cathy Deng <cathyd125@gmail.com>
2014-08-28 14:20:58 -0500

    add us50 addresses to tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -13,8 +13,34 @@ class TestSynthetic(object) :
             yield equals, address_text, labels_pred, labels_true
 
 
+class TestUS50_2(object) :
+    def test_Parser(self):
+
+        test_file = 'training/test_data/us50_test_tagged.xml'
+
+        for address_text, components in parseTrainingData(test_file) :
+            _, labels_t = zip(*components)
+            _, labels = zip(*parse(address_text))
+            
+            yield fuzzyEquals, address_text, labels_t, labels
+
+
+
 def equals(addr, 
            labels_pred, 
            labels_true) :
     assert labels_pred == labels_true
 
+def fuzzyEquals(addr, 
+           labels_true, 
+           labels_pred) :
+    labels = []
+    fuzzy_labels = []
+    for label in labels_pred:
+        if label == 'StreetNamePostType' or label == 'StreetNamePreDirectional':
+            fuzzy_labels.append('StreetName')
+        else:
+            fuzzy_labels.append(label)
+    for label in labels_true:
+        labels.append(label)
+    assert fuzzy_labels == labels
