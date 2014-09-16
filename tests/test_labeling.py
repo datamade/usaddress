48e0f95c2fe19b6048301ee914ee118a7c551ce9
Cathy Deng <cathyd125@gmail.com>
2014-09-16 16:55:56 -0500

    add tests for openaddress us-ia-linn

--- tests/test_labeling.py
+++ tests/test_labeling.py
@@ -25,6 +25,18 @@ class TestUS50_2(object) :
             
             yield fuzzyEquals, address_text, labels_pred, labels_true
 
+
+class TestOpenaddress(object) :
+    def test_us_ia_linn(self) :
+
+        test_file = 'training/training_data/openaddress_us_ia_linn.xml'
+
+        for address_text, components in parseTrainingData(test_file) :
+            _, labels_true = zip(*components)
+            _, labels_pred = zip(*parse(address_text))
+            yield equals, address_text, labels_pred, labels_true
+
+
 def equals(addr, 
            labels_pred, 
            labels_true) :
