f2338dfd28efbf2a418c2a16d5e959ff6e8f48cc
Cathy Deng <cathyd125@gmail.com>
2014-10-09 16:09:20 -0500

    limit the addresses that are tested on in labeling test

--- tests/test_labeling.py
+++ tests/test_labeling.py
@@ -1,31 +1,22 @@
 from usaddress import parse
 import training
 from training.training import parseTrainingData
+import unittest
 
 #tests to ensure that the parser maintains performance on various simple address patterns
+class TestManualAddr2XML(unittest.TestCase) :
+
+    def test_simple_addresses(self):
+        test_file = 'training/test_data/simple_address_patterns.xml'
+
+        for address_text, components in parseTrainingData(test_file) :
+            _, labels_true = zip(*components)
+            _, labels_pred = zip(*parse(address_text))
+            yield equals, address_text, labels_pred, labels_true
+
 
 def equals(addr, 
            labels_pred, 
            labels_true) :
     print "ADDRESS: ", addr
     assert labels_pred == labels_true
-
-def fuzzyEquals(addr, 
-                labels_pred,
-                labels_true) : 
-    labels = []
-    fuzzy_labels = []
-    for label in labels_pred:
-        if label.startswith('StreetName') :
-            fuzzy_labels.append('StreetName')
-        elif label.startswith('AddressNumber') :
-            fuzzy_labels.append('AddressNumber')
-        else:
-            fuzzy_labels.append(label)
-    for label in labels_true:
-        labels.append(label)
-    print "ADDRESS:    ", addr
-    print "fuzzy pred: ", fuzzy_labels
-    print "true:       ", labels
-
-    assert fuzzy_labels == labels
