5ccba8c5396d0e9a63cfe098e867d1f8fb69b2da
Cathy Deng <cathyd125@gmail.com>
2014-12-12 15:28:44 -0600

    unit test classes

--- tests/test_labeling.py
+++ tests/test_labeling.py
@@ -1,42 +1,40 @@
-from usaddress import parse
+from usaddress import parse, GROUP_LABEL
 from parserator.training import readTrainingData
 import unittest
 
-mod = __import__("usaddress")
+class TestSimpleAddresses(unittest.TestCase) :
+    def test_simple_addresses(self):
+        test_file = 'measure_performance/test_data/simple_address_patterns.xml'
+        data = list(readTrainingData([test_file], GROUP_LABEL))
 
+        for labeled_address in data :
+            address_text, components = labeled_address
+            _, labels_true = zip(*components)
+            _, labels_pred = zip(*parse(address_text))
+            yield equals, address_text, labels_pred, labels_true
 
-def test_simple_addresses():
-    test_file = 'measure_performance/test_data/simple_address_patterns.xml'
-    data = list(readTrainingData([test_file], mod))
+class TestSyntheticAddresses(unittest.TestCase) :
+    def test_synthetic_addresses(self):
 
-    for labeled_address in data :
-        address_text, components = labeled_address
-        _, labels_true = zip(*components)
-        _, labels_pred = zip(*parse(address_text))
-        yield equals, address_text, labels_pred, labels_true
+        test_file = 'measure_performance/test_data/synthetic_osm_data.xml'
+        data = list(readTrainingData([test_file], GROUP_LABEL))
 
+        for labeled_address in data :
+            address_text, components = labeled_address
+            _, labels_true = zip(*components)
+            _, labels_pred = zip(*parse(address_text))
+            yield equals, address_text, labels_pred, labels_true
 
-def test_synthetic_addresses():
+class TestUS50Addresses(unittest.TestCase) :
+    def test_us50(self):
+        test_file = 'measure_performance/test_data/us50_test_tagged.xml'
+        data = list(readTrainingData([test_file], GROUP_LABEL))
 
-    test_file = 'measure_performance/test_data/synthetic_osm_data.xml'
-    data = list(readTrainingData([test_file], mod))
-
-    for labeled_address in data :
-        address_text, components = labeled_address
-        _, labels_true = zip(*components)
-        _, labels_pred = zip(*parse(address_text))
-        yield equals, address_text, labels_pred, labels_true
-
-
-def test_us50():
-    test_file = 'measure_performance/test_data/us50_test_tagged.xml'
-    data = list(readTrainingData([test_file], mod))
-
-    for labeled_address in data :
-        address_text, components = labeled_address
-        _, labels_true = zip(*components)
-        _, labels_pred = zip(*parse(address_text))
-        yield fuzzyEquals, address_text, labels_pred, labels_true
+        for labeled_address in data :
+            address_text, components = labeled_address
+            _, labels_true = zip(*components)
+            _, labels_pred = zip(*parse(address_text))
+            yield fuzzyEquals, address_text, labels_pred, labels_true
 
 
 def equals(addr, 
