0dc31218f4e19091b33596014173db5d9a9bbdd6
Forest Gregg <fgregg@uchicago.edu>
2015-03-03 19:02:11 -0600

    fix test

--- tests/test_tagging.py
+++ tests/test_tagging.py
@@ -5,7 +5,6 @@ class TestTagging(unittest.TestCase) :
 
     def test_broadway(self) :
         s1 = '1775 Broadway And 57th, Newyork NY'
-        print usaddress.tag(s1)
-        assert 1 == 0
+        usaddress.tag(s1)
 
 
